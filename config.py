a7329c91ff920fb1e163bcec0b447a0d9522d38c
safran kankol <safrankankol2@gmail.com>
2026-09-19 01:30:42 +0530

    ui?

--- config.py
+++ config.py
@@ -3,7 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-change-me")
     BASE_URL = os.environ.get("BASE_URL", "http://localhost:5000")
